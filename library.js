239ced0b1313359ed14036015cce82217e9df347
Holly <hl4god@gmail.com>
2016-06-02 10:43:19 +0800

    update

--- library.js
+++ library.js
@@ -22,66 +22,66 @@ Plugin.award = function (params, callback) {
 	//  console.log("params 20:",params);
 	var uid = params.uid;
 	var credit = parseInt(params.reward.reputation);
-	async.waterfall([
-			function (callback) {
-				User.getUserFields(uid, ['username'], callback);
-			},
-			function (user, callback) {
-				console.log("user:",user);
-				async.parallel([
-					function (callback) { //加经验
-						awardCredit({
-							uid: user.username,
-							credit: credit
-						}, function (err, res) {
-							console.log("res:36:",res);
-							//奖励积分经验结果
-							var result = !err && (res.r && res.r.code === 504) ? 'sucesss' : 'fail';
-							awardLog(uid, {
-								credit: credit,
-								result: result,
-								createdAt: Date.now()
-							}, callback);
-							//{"locale":null,"r":{"code":504,"text":"成功"}}
-						});
-					},
-					function (callback) {//在freebacking加威望
-						awardReputationToFreebacking({
-							pid: user.username,
-							increment: credit
-						}, function (err, res) {
-							console.log("res:52:",res);
-							//奖励积分经验结果
-							var result = !err && (res.r && res.r.code === 504) ? 'sucesss' : 'fail';
-							awardReputationLog(uid, {
-								credit: credit,
-								result: result,
-								createdAt: Date.now()
-							}, callback);
-							//{"locale":null,"r":{"code":504,"text":"成功"}}
-						});
-					}
-				], callback);
-
-			}
-		],
-		function (err) {
-			console.error("err36:", err);
-			if (callback) {
-				callback(err);
-			}
-
-		});
-	// User.getUserFields(uid, ['username'], function (err, user) {
-	// 	if (err && callback) {
-	// 		return callback(err);
-	// 	}
-	// 	awardCredit({uid: user.username, credit: credit}, function (err) {
+	// async.waterfall([
+	// 		function (callback) {
+	// 			User.getUserFields(uid, ['username'], callback);
+	// 		},
+	// 		function (user, callback) {
+	// 			console.log("user:",user);
+	// 			async.parallel([
+	// 				function (callback) { //加经验
+	// 					awardCredit({
+	// 						uid: user.username,
+	// 						credit: credit
+	// 					}, function (err, res) {
+	// 						console.log("res:36:",res);
+	// 						//奖励积分经验结果
+	// 						var result = !err && (res.r && res.r.code === 504) ? 'sucesss' : 'fail';
+	// 						awardLog(uid, {
+	// 							credit: credit,
+	// 							result: result,
+	// 							createdAt: Date.now()
+	// 						}, callback);
+	// 						//{"locale":null,"r":{"code":504,"text":"成功"}}
+	// 					});
+	// 				},
+	// 				function (callback) {//在freebacking加威望
+	// 					awardReputationToFreebacking({
+	// 						pid: user.username,
+	// 						increment: credit
+	// 					}, function (err, res) {
+	// 						console.log("res:52:",res);
+	// 						//奖励积分经验结果
+	// 						var result = !err && (res.r && res.r.code === 504) ? 'sucesss' : 'fail';
+	// 						awardReputationLog(uid, {
+	// 							credit: credit,
+	// 							result: result,
+	// 							createdAt: Date.now()
+	// 						}, callback);
+	// 						//{"locale":null,"r":{"code":504,"text":"成功"}}
+	// 					});
+	// 				}
+	// 			], callback);
+	//
+	// 		}
+	// 	],
+	// 	function (err) {
+	// 		console.error("err36:", err);
 	// 		if (callback) {
 	// 			callback(err);
 	// 		}
 	// 	});
-	// });
+	//
+	User.getUserFields(uid, ['username'], function (err, user) {
+		if (err && callback) {
+			return callback(err);
+		}
+		awardCredit({uid: user.username, credit: credit}, function (err) {
+			if (callback) {
+				callback(err);
+			}
+		});
+	});
 };
 
 //记录威望奖励积分请求的结果
